97ceefef457a71377b9a7ab30cb059b8cfd3b998
SandhyaReddy <158995969+SandhyaReddy-2713@users.noreply.github.com>
2025-01-08 23:24:32 +0530

    Update instance.tf

--- ec2/instance.tf
+++ ec2/instance.tf
@@ -1,13 +1,12 @@
-# resource "aws_instance" "ec2_instance" {
-#   ami           = "ami-01816d07b1128cd2d"  # Replace with your AMI ID
-#   instance_type = "t2.micro"               # Replace with your instance type
-#   availability_zone = "us-east-1a"         # Specify the AZ
+resource "aws_instance" "ec2_instance" {
+ami           = "ami-01816d07b1128cd2d"  # Replace with your AMI ID
+instance_type = "t2.micro"               # Replace with your instance type
+availability_zone = "us-east-1a"         # Specify the AZ
 
-#   # Reference the created subnet and security group
-#   subnet_id              = "subnet-0114f0705067856b3"
-#   security_groups = ["sg-046df65994fef06e4"]
-
-#   tags = {
-#     Name = "task-instance"
-#   }
-# }
+# Reference the created subnet and security group
+subnet_id              = "subnet-0114f0705067856b3"   #replace with subnet-id
+security_groups = ["sg-046df65994fef06e4"]            #replace with security-group-id
+tags = {
+Name = "task-instance"
+   }
+ }
