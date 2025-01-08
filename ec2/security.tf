db03f4b3b3c41f3b265a0c6a90c49ec858227b00
sandhya <sandhya@gmail.com>
2025-01-08 23:07:18 +0530

    added files

---
+++ ec2/security.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "web_sg" {
+  name        = "task-security"
+  description = "Allow HTTP and HTTPS traffic"
+  vpc_id = "vpc-062b098ba027603d8"
+  ingress {
+    description      = "Allow HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "Allow HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "Allow SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  egress {
+    description      = "Allow all outbound traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
